a55b303c3657b8975a8b02f14e7bf886787544c9
Chris <chrishum17@gmail.com>
2019-09-16 10:54:33 -0400

    template and sketches folders for organization

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('Rockets.html')
+
+@app.route('/sketches/<path:path>')
+def send_sketches(path):
+    return send_from_directory('sketches', path)
 
 if __name__ == '__main__':
   app.run()
